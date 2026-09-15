6fd01e20c44d2de31ff56e6aa64a60f9dd7bd521
VictorDAW2627 <vicarrper@alu.edu.gva.es>
2026-09-15 14:27:30 -0400

    15/9/26

---
+++ Tema1/Ejercicio4.php
@@ -0,0 +1,11 @@
+<?php
+    $curriculum_es = "Me llamo Victor y "."he estudiado DAW."."<br><br>"."Se hablar ingles, valenciano, español y un poco de japones";
+    $curriculum_va = "Em dic Victor i "."he estudiat DAW."."<br><br>"."Sé parlar anglés, valencià, espanyol i un poc de japones";
+    $curriculum_en = "My name is Victor and "."I've studied DAW."."<br><br>"."I speak English, Valencian, Spanish, and a little Japanese.";
+
+    $idioma = "es";
+
+    $curriculum = "curriculum_".$idioma;
+
+    echo $$curriculum;
+?>
