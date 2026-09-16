0a68da5d84a608d4f74614f7f9c1980c98f6e81c
VictorDAW2627 <vicarrper@alu.edu.gva.es>
2026-09-16 09:35:23 -0400

    16/9/26

---
+++ Tema1/Ejercicio4.2.php
@@ -0,0 +1,12 @@
+<?php
+
+    $nota1 = 7;
+    $nota2 = 5.5;
+
+    if ($nota1>$nota2) {
+        echo "La 1ª nota es mayor (".$nota1.")";
+    }else{
+        echo "La 2ª nota es mayor (".$nota2.")";
+    }
+
+?>
