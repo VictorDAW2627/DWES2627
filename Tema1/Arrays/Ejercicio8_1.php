f45a79a4fca446798888644c2fc6fd6a56e12bf9
VictorDAW2627 <vicarrper@alu.edu.gva.es>
2026-09-17 15:52:57 -0400

    18/9/26

--- Tema1/Arrays/Ejercicio8_1.php
+++ Tema1/Arrays/Ejercicio8_1.php
@@ -2,14 +2,17 @@
 
     $numeros[0] = rand(0, 99);   
     $repetido = FALSE;
+    $menor;
+    $mayor;
+    $media = 0;
 
-    for ($i=1; $i <50 ; $i++) {
+    for ($i=1; $i < 50 ; $i++) {
         $repetido = FALSE;
 
         $random = rand(0, 99);
 
         for ($j=0; $j < count($numeros); $j++) { 
-            if ($numeros[j]==$random) {
+            if ($numeros[$j]==$random) {
                 $i--;
                 $repetido = TRUE;
             }
@@ -20,8 +23,35 @@
         }
     }
 
-    for ($k=0; $k < count($numeros); $++) { 
-        echo $numeros[$i].",";
+    for ($k=0; $k < count($numeros); $k++) { 
+        echo $numeros[$k].",";
     }
 
+    sort($numeros);
+    echo "<br><br>";
+
+    for ($l=0; $l < count($numeros); $l++) { 
+        echo $numeros[$l].",";
+    }
+
+    $mayor = $numeros[count($numeros)-1];
+
+    echo "<br><br>";
+
+    echo "El numero mayor es ".$mayor;
+
+    $menor = $numeros[0];
+
+    echo "<br><br>";
+
+    echo "El numero menor es ".$menor;
+
+    for ($p=0; $p < count($numeros); $p++) { 
+        $media+=$numeros[$p];
+    }
+    $media/=count($numeros);
+
+    echo "<br><br>";
+
+    echo "La media es ".$media;
 ?>
